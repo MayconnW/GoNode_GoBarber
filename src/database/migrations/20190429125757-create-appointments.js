e33f1105597cbdf0834a694837eac4b7038c22d2
Mayconn Oliveira <mayconnwgo@gmail.com>
2019-05-02 21:18:43 -0300

    Start appointment develop

---
+++ src/database/migrations/20190429125757-create-appointments.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('appointments')
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -15,6 +17,8 @@ routes.use((req, res, next) => {
 
   return next()
 })
+
+routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
@@ -24,9 +28,7 @@ routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.use('/app', authMiddleware)
 
-routes.get('/app/dashboard', (req, res) => {
-  res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 routes.get('/app/logout', SessionController.destroy)
 
