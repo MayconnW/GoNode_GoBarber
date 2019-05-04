09581b877700c29dc877734b7ae0a476f6e1ab54
Mayconn Oliveira <mayconnwgo@gmail.com>
2019-05-04 14:38:00 -0300

    Make an appointment

--- src/database/migrations/20190429125757-create-appointments.js
+++ src/database/migrations/20190429125757-create-appointments.js
@@ -12,6 +12,28 @@ module.exports = {
       date: {
         allowNull: false,
         type: Sequelize.DATE
+      },
+      customer_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     })
   },
